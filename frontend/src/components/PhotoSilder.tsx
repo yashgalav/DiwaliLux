d1b6ce443519cb2a780a5c25b3b0cc2072a9ba15
yashgalav <yashgalav12345@gmail.com>
2025-08-31 12:53:56 +0530

    change slider size

--- frontend/src/components/PhotoSilder.tsx
+++ frontend/src/components/PhotoSilder.tsx
@@ -37,7 +37,7 @@ export default function PhotoSilder({ Sliders }: SliderProps) {
 
 
     return (
-        <div className='max-w-[1000px] h-[680px] w-full m-auto py-16 px-4 relative group'>
+        <div className='max-w-[680px] h-[1000px] w-full m-auto py-16 px-4 relative group'>
             <div
                 style={{ backgroundImage: `url(${Sliders[currentIndex].url})` }}
                 className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
